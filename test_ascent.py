931bad68b6667f1e3cf7eb5b3020d779d7cea88d
matt-saenz <55054385+matt-saenz@users.noreply.github.com>
2022-10-18 08:45:40 -0500

    Add testing for Ascent objects

--- test_ascent.py
+++ test_ascent.py
@@ -1,10 +1,26 @@
 """Testing for the ascent module."""
 
 
-from datetime import date
+from datetime import date, timedelta
 import unittest
 
-from ascent import Ascent, AscentLog, AscentLogError
+from ascent import Ascent, AscentError, AscentLog, AscentLogError
+
+
+class TestAscent(unittest.TestCase):
+    def test_grade(self):
+        bad_grades = ["5.9+", "5.10", "5.11a/b", "5.12-"]
+
+        for bad_grade in bad_grades:
+            with self.assertRaises(AscentError):
+                Ascent("Some Route", bad_grade, "Some Crag", date.today())
+
+    def test_date(self):
+        bad_dates = ["2022-10-17", date.today() + timedelta(days=1)]
+
+        for bad_date in bad_dates:
+            with self.assertRaises(AscentError):
+                Ascent("Some Route", "5.7", "Some Crag", bad_date)
 
 
 class TestAscentLogMethods(unittest.TestCase):
